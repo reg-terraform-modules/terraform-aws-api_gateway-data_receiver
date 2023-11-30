ae3b3a7349cad2f8d31ea40245accfb7b6de18a5
Samuel <61912873+samuelberntzen@users.noreply.github.com>
2023-11-30 16:49:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,6 +127,6 @@ resource "aws_api_gateway_integration_response" "cors_options" {
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'"
-        "method.response.header.Access-Control-Allow-Origin"
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
     }
 }
